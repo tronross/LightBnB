b19fd8e84a07f36e5427ebe34e6469135a879ac5
tronross <john_ross@analogforever.ca>
2022-12-08 19:55:46 -0500

    Add TABLE properties to 01_schema

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -22,4 +22,21 @@ CREATE TABLE property_reviews (
   message TEXT
 );
 
-
+CREATE TABLE properties (
+  id INTEGER PRIMARY_KEY NOT NULL,
+  owner_id INTEGER FOREIGN_KEY REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN
+);
