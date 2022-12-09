a94bf73b5209502598636cdbbe1af4bc28e69323
tronross <john_ross@analogforever.ca>
2022-12-08 19:47:54 -0500

    Add TABLE property_reviews to 01_schema

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,14 +1,25 @@
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY_KEY NOT NULL,
   name VARCHAR(255),
   email VARCHAR(255),
   password VARCHAR(255)
 );
 
 CREATE TABLE reservations (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY_KEY NOT NULL,
   start_date DATE,  
   end_date DATE,
   property_id INTEGER FOREIGN_KEY REFERENCES properties(id) ON DELETE CASCADE,
-  guest_id INTEGER FOREIGN_KEY REFERENCES users(id)
-)
+  guest_id INTEGER FOREIGN_KEY REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE property_reviews (
+  id INTEGER PRIMARY_KEY NOT NULL,
+  guest_id INTEGER FOREIGN_KEY REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER FOREIGN_KEY REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER FOREIGN_KEY REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT,
+  message TEXT
+);
+
+
