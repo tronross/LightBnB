853d99edb117e26e181d47eb94349c24d37d19a6
tronross <john_ross@analogforever.ca>
2022-12-09 16:51:24 -0500

    Add users seed

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,10 @@
+-- //////////////////////////////
+-- SEEDS FOR TABLES
+-- //////////////////////////////
+
+
+INSERT INTO users (name, email, password)
+VALUES ('Oswalt Ruthschager', 'ossie@teddybearz.net', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Caroline Pharoah', 'car123@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Phoebe Fuzz', 'huzzah@phoebe.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+
